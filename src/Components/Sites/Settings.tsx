2f0e6dada1755931c07d7886896ab96903b5daf3
Herobone <theherobone@gmail.com>
2020-12-19 17:57:00 +0100

    Removed OnlyAuthed from Settings

--- src/Components/Sites/Settings.tsx
+++ src/Components/Sites/Settings.tsx
@@ -1,10 +1,9 @@
-import React, { Component, ReactElement } from 'react'
+import React, {Component, ReactElement} from 'react'
 import Column from '../Visuals/Column';
 import Dropdown from '../Visuals/Dropdown';
 import firebase from 'firebase';
-import { FormattedMessage } from 'react-intl';
-import { Link } from 'react-router-dom';
-import OnlyAuthed from '../Functional/OnlyAuthed';
+import {FormattedMessage} from 'react-intl';
+import {Link} from 'react-router-dom';
 import {Alert} from '../../helper/AlertTypes';
 
 interface Props {
@@ -20,7 +19,7 @@ export class Settings extends Component<Props> {
             "de": "Deutsch",
             "en": "English"
         };
-        
+
         const currentUser = firebase.auth().currentUser;
 
         let userName = "Logged Out";
@@ -28,21 +27,19 @@ export class Settings extends Component<Props> {
             userName = currentUser.displayName;
         }
         return (
-            <OnlyAuthed className="w3-container w3-content app-content">
-                <Column additionalClasses="app-content">
-                    <h1><FormattedMessage id="account.navigation.settings" /></h1>
-                    <h3>Hi {userName}!</h3>
-                    <hr />
-                    <h5><FormattedMessage id="settings.labels.selectlanguage" /></h5>
-                    <Dropdown callback={this.props.changeLanguage} content={options} selected={this.props.currentLocale} />
-                    <hr />
-                    <br />
-                    <Link to="/logout" className="w3-bar-item w3-button w3-red">
-                        <FormattedMessage id="account.actions.logout" />
-                    </Link>
-                    <br />
-                </Column>
-            </OnlyAuthed>
+            <Column additionalClasses="app-content">
+                <h1><FormattedMessage id="account.navigation.settings"/></h1>
+                <h3>Hi {userName}!</h3>
+                <hr/>
+                <h5><FormattedMessage id="settings.labels.selectlanguage"/></h5>
+                <Dropdown callback={this.props.changeLanguage} content={options} selected={this.props.currentLocale}/>
+                <hr/>
+                <br/>
+                <Link to="/logout" className="w3-bar-item w3-button w3-red">
+                    <FormattedMessage id="account.actions.logout"/>
+                </Link>
+                <br/>
+            </Column>
         )
     }
 }
