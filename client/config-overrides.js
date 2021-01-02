05040a663453fff1cff4d3794be6ec29fc4ca3cc
Herobone <theherobone@gmail.com>
2021-01-02 15:16:09 +0100

    Overrides for webpack

---
+++ client/config-overrides.js
@@ -0,0 +1,93 @@
+/*****************************
+ * Sober Sailor - The online Party Game
+ * Copyright (c) 2021.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+const TerserPlugin = require("terser-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const safePostCssParser = require("postcss-safe-parser");
+
+module.exports = {
+    // The Webpack config to use when compiling your react app for development or production.
+    webpack: function (config, env) {
+        // ...add your webpack config
+        // Source maps are resource heavy and can cause out of memory issue for large source files.
+        const shouldUseSourceMap = process.env.GENERATE_SOURCEMAP !== "false";
+        config.optimization.minimizer = [
+            new TerserPlugin({
+                sourceMap: shouldUseSourceMap,
+                terserOptions: {
+                    parse: {
+                        // We want terser to parse ecma 8 code. However, we don't want it
+                        // to apply any minification steps that turns valid ecma 5 code
+                        // into invalid ecma 5 code. This is why the 'compress' and 'output'
+                        // sections only apply transformations that are ecma 5 safe
+                        // https://github.com/facebook/create-react-app/pull/4234
+                        ecma: 8,
+                    },
+                    compress: {
+                        ecma: 5,
+                        warnings: false,
+                        // Disabled because of an issue with Uglify breaking seemingly valid code:
+                        // https://github.com/facebook/create-react-app/issues/2376
+                        // Pending further investigation:
+                        // https://github.com/mishoo/UglifyJS2/issues/2011
+                        comparisons: false,
+                        // Disabled because of an issue with Terser breaking valid code:
+                        // https://github.com/facebook/create-react-app/issues/5250
+                        // Pending further investigation:
+                        // https://github.com/terser-js/terser/issues/120
+                        inline: 2,
+                        pure_funcs: ["console.log"],
+                    },
+                    mangle: {
+                        safari10: true,
+                    },
+                    // Added for profiling in devtools
+                    keep_classnames: false,
+                    keep_fnames: false,
+                    output: {
+                        ecma: 5,
+                        comments: false,
+                        // Turned on because emoji and regex is not minified properly using default
+                        // https://github.com/facebook/create-react-app/issues/2488
+                        ascii_only: true,
+                    },
+                },
+            }),
+            // This is only used in production mode
+            new OptimizeCSSAssetsPlugin({
+                cssProcessorOptions: {
+                    parser: safePostCssParser,
+                    map: shouldUseSourceMap
+                        ? {
+                              // `inline: false` forces the sourcemap to be output into a
+                              // separate file
+                              inline: false,
+                              // `annotation: true` appends the sourceMappingURL to the end of
+                              // the css file, helping the browser find the sourcemap
+                              annotation: true,
+                          }
+                        : false,
+                },
+                cssProcessorPluginOptions: {
+                    preset: ["default", { minifyFontValues: { removeQuotes: false } }],
+                },
+            }),
+        ];
+        return config;
+    },
+};
