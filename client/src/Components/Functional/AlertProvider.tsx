08e10a06ba8236b680120208a2ca02dd383c66d2
Herobone <theherobone@gmail.com>
2021-01-03 18:46:37 +0100

    Max count and lifetime for Alerts

--- client/src/Components/Functional/AlertProvider.tsx
+++ client/src/Components/Functional/AlertProvider.tsx
@@ -21,66 +21,81 @@ import { Alert as IAlert } from "../../helper/AlertTypes";
 import { Alert } from "../Visuals/Alert";
 
 interface State {
-  errorToDisplay: Map<number, ReactElement>;
-  lastIndex: number;
+    errorToDisplay: Map<number, ReactElement>;
+    lastIndex: number;
 }
 
 interface Props {}
 
 export class AlertProvider extends Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      errorToDisplay: new Map<number, ReactElement>(),
-      lastIndex: 0,
+    maxAlerts = 5;
+
+    alertLifetime = 5000;
+
+    clearAlerts: Map<number, NodeJS.Timeout> = new Map();
+
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+            errorToDisplay: new Map<number, ReactElement>(),
+            lastIndex: 0,
+        };
+    }
+
+    componentWillUnmount(): void {
+        this.clearAlerts.forEach((timeout) => clearTimeout(timeout));
+    }
+
+    prepareAlerts = (): ReactElement[] => {
+        const values: ReactElement[] = [];
+        this.state.errorToDisplay.forEach((val: ReactElement): void => {
+            values.push(val);
+        });
+        return values;
     };
-    this.createAlert = this.createAlert.bind(this);
-  }
 
-  createAlert(type: IAlert, message: string | ReactElement, header?: ReactElement): void {
-    const { errorToDisplay, lastIndex } = this.state;
-    const alertIndex = lastIndex + 1;
-    const al = (
-      <Alert
-        key={`alert${alertIndex}`}
-        type={type}
-        header={header}
-        clear={() => {
-          // eslint-disable-next-line react/no-access-state-in-setstate
-          const errorToDisplayClear = this.state.errorToDisplay;
+    clearAlert = (alertIndex: number): void => {
+        this.setState((prev) => {
+            const errorToDisplayClear = prev.errorToDisplay;
 
-          errorToDisplayClear.delete(alertIndex);
-          this.setState({
-            errorToDisplay: errorToDisplayClear,
-          });
-        }}
-      >
-        {message}
-      </Alert>
-    );
-    errorToDisplay.set(alertIndex, al);
-    this.setState({
-      errorToDisplay,
-      lastIndex: alertIndex,
-    });
-  }
+            errorToDisplayClear.delete(alertIndex);
+            return {
+                errorToDisplay: errorToDisplayClear,
+            };
+        });
+        this.clearAlerts.delete(alertIndex);
+    };
 
-  prepareAlerts(): ReactElement[] {
-    const vals: ReactElement[] = [];
-    this.state.errorToDisplay.forEach((val: ReactElement): void => {
-      vals.push(val);
-    });
-    return vals;
-  }
+    createAlert = (type: IAlert, message: string | ReactElement, header?: ReactElement): void => {
+        const { errorToDisplay, lastIndex } = this.state;
+        const alertIndex = lastIndex + 1;
+        const al = (
+            <Alert key={`alert${alertIndex}`} type={type} header={header} clear={() => this.clearAlert(alertIndex)}>
+                {message}
+            </Alert>
+        );
+        if (errorToDisplay.size + 1 > this.maxAlerts) {
+            errorToDisplay.delete(lastIndex - (this.maxAlerts - 1));
+        }
+        this.clearAlerts.set(
+            alertIndex,
+            setTimeout(() => this.clearAlert(alertIndex), this.alertLifetime),
+        );
+        errorToDisplay.set(alertIndex, al);
+        this.setState({
+            errorToDisplay,
+            lastIndex: alertIndex,
+        });
+    };
 
-  render(): JSX.Element {
-    return (
-      <div>
-        <div className="w3-container w3-content">
-          <div className="alert-area">{this.prepareAlerts()}</div>
-        </div>
-        {this.props.children}
-      </div>
-    );
-  }
+    render(): JSX.Element {
+        return (
+            <div>
+                <div className="w3-container w3-content">
+                    <div className="alert-area">{this.prepareAlerts()}</div>
+                </div>
+                {this.props.children}
+            </div>
+        );
+    }
 }
