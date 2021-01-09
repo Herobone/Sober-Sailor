9f6d332731c19f0c5313c3cad612842cb2758183
Herobone <theherobone@gmail.com>
2021-01-09 16:22:01 +0100

    Valid test

--- client/src/tests/Index.test.tsx
+++ client/src/tests/Index.test.tsx
@@ -19,13 +19,14 @@
 // eslint-disable-next-line no-use-before-define
 import React from "react";
 import { render } from "@testing-library/react";
-import firebase from "firebase";
+import firebase from "firebase/app";
+import "firebase/auth";
 import { act as domAct } from "react-dom/test-utils";
 import { unmountComponentAtNode } from "react-dom";
 import { LanguageContainer } from "../translations/LanguageContainer";
 import { config } from "../helper/config";
 import { AlertProvider } from "../Components/Functional/AlertProvider";
-import { Alert, Error, Warning } from "../helper/AlertTypes";
+import { Alert, AlertContextType, Error, Warning } from "../helper/AlertTypes";
 import { Routed } from "../Components/Functional/Routed";
 
 firebase.initializeApp(config);
@@ -34,51 +35,59 @@ firebase.auth().setPersistence(firebase.auth.Auth.Persistence.NONE);
 let container: HTMLDivElement | undefined;
 
 beforeEach(() => {
-  // setup a DOM element as a render target
-  container = document.createElement("div");
-  document.body.append(container);
+    // setup a DOM element as a render target
+    container = document.createElement("div");
+    document.body.append(container);
 });
 
 afterEach(() => {
-  // cleanup on exiting
-  if (container) unmountComponentAtNode(container);
-  container?.remove();
-  container = undefined;
+    // cleanup on exiting
+    if (container) unmountComponentAtNode(container);
+    container?.remove();
+    container = undefined;
 });
 
 // eslint-disable-next-line jest/expect-expect
 test("Renders the App without GAPI", () => {
-  const okToCall = jest.fn();
+    const okToCall = jest.fn();
 
-  domAct(() => {
-    render(
-      <React.StrictMode>
-        <LanguageContainer>
-          <AlertProvider>
-            <Routed changeLanguage={okToCall} currentLocale="en" createAlert={okToCall} />
-          </AlertProvider>
-        </LanguageContainer>
-      </React.StrictMode>,
-    );
-  });
+    domAct(() => {
+        render(
+            <React.StrictMode>
+                <LanguageContainer>
+                    <AlertProvider>
+                        <Routed changeLanguage={okToCall} currentLocale="en" />
+                    </AlertProvider>
+                </LanguageContainer>
+            </React.StrictMode>,
+        );
+    });
 });
 
 test("Renders the Router and looks for Alerts", () => {
-  const neverCallThis = jest.fn();
-  const alertFN = (type: Alert): void => {
-    if (type instanceof Warning || type instanceof Error) {
-      neverCallThis();
-    }
-  };
+    const neverCallThis = jest.fn();
+    const alertFN = (type: Alert): void => {
+        if (type instanceof Warning || type instanceof Error) {
+            neverCallThis();
+        }
+    };
 
-  const okToCall = jest.fn();
+    const AlertContext = React.createContext<AlertContextType>({
+        createAlert: (type: Alert) => {
+            alertFN(type);
+        },
+    });
 
-  domAct(() => {
-    render(
-      <LanguageContainer>
-        <Routed changeLanguage={okToCall} currentLocale="en" createAlert={alertFN} />
-      </LanguageContainer>,
-    );
-  });
-  expect(neverCallThis).not.toBeCalled();
+    const okToCall = jest.fn();
+
+    domAct(() => {
+        render(
+            <AlertContext.Provider value={{ createAlert: alertFN }}>
+                <LanguageContainer>
+                    <Routed changeLanguage={okToCall} currentLocale="en" />
+                </LanguageContainer>
+            </AlertContext.Provider>,
+        );
+    });
+    expect(neverCallThis).not.toBeCalled();
 });
