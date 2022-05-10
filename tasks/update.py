773d9d31b561ab323e81cedc06aa4d7fc96307c5
Julius G <theherobone@gmail.com>
2022-05-10 17:04:03 +0200

    📂 Update Question source

--- tasks/update.py
+++ tasks/update.py
@@ -2,14 +2,25 @@ import json
 
 tasks = {}
 
-file = "./wouldyourather/en.json"
+file = "./wouldyourather/de.json"
+out = "./wouldyourather/de.new.json"
 
 with open(file) as json_file:
     data = json.load(json_file)
     counter = 0
     for task in data:
-        tasks[str(counter)] = task
+        datum = data[str(counter)]
+        print(datum)
+        tmp = {}
+        tmp["question"] = datum["question"]
+        tmp2 = {}
+        for answer in datum["answers"]:
+            tmp2[answer["id"]] = answer["answer"]
+            print(answer)
+        tmp["anwers"] = tmp2
+        tasks[counter] = tmp
         counter += 1
     
-with open(file, "w") as outfile:
+print(tasks)
+with open(out, "w") as outfile:
     json.dump(tasks, outfile, ensure_ascii=False, indent=4)
