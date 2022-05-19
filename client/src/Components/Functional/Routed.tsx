6eddbec7685dd58b642f00128a561d233131c03a
Julius G <theherobone@gmail.com>
2022-05-19 19:22:52 +0200

    📜 Update License to AGPLv3

--- client/src/Components/Functional/Routed.tsx
+++ client/src/Components/Functional/Routed.tsx
@@ -1,19 +1,17 @@
-/** ***************************
+/*****************************
  * Sober Sailor - The online Party Game
- * Copyright (c) 2020.
+ * Copyright (c) 2020-2022.
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU Affero General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Affero General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
  */
 
 import React, { Suspense, lazy } from "react";
@@ -26,6 +24,7 @@ import { MixedGameProvider } from "./MixedGameProvider";
 import { Logout } from "./Logout";
 
 const Mixed = lazy(() => import("../Sites/Gamemodes/Mixed"));
+const DataProtection = lazy(() => import("../Sites/DataProtection"));
 
 export function Routed(): JSX.Element {
     const classes = useDefaultStyles();
@@ -43,6 +42,8 @@ export function Routed(): JSX.Element {
                         {/*<Route path="/login">
                             <Login />
                         </Route>*/}
+
+                        <Route path="privacy-policy" element={<DataProtection />} />
 
                         <Route path="logout" element={<Logout />} />
 
